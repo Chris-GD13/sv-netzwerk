3e4cf0ab8fb21134c913f4ed3cd7d980a0ca2daf
Chris-GD13 <chriswaechter@me.com>
2026-09-04 21:37:55 +0200

    Add portal telephone center (4/6)

--- sv-netzwerk/tests/case_search_test.php
+++ sv-netzwerk/tests/case_search_test.php
@@ -19,6 +19,8 @@ $meta = [
     'ort' => 'Freudenstadt',
     'schaden_strasse' => 'Höhenweg 7',
     'versicherungsschein_nr' => 'SV-4711',
+    'telefon' => '+49 7441 123456',
+    'telefonkontakte' => [['name' => 'Hausmeister', 'nummer' => '0711 9876543']],
 ];
 $text = caseSearchText($meta, '26-160966-8 - WEG 30086');
 
@@ -27,6 +29,7 @@ expectSearch(caseSearchMatches($text, 'rheinland 72250'), 'Mehrere Suchwörter m
 expectSearch(caseSearchMatches($text, 'Höhenweg'), 'Umlaute in der Suche müssen normalisiert werden.');
 expectSearch(caseSearchMatches($text, 'hoehenweg'), 'Umschrift eines Umlauts muss denselben Treffer liefern.');
 expectSearch(caseSearchMatches($text, '26-160966'), 'Teil einer Schaden-Nr. muss gefunden werden.');
+expectSearch(caseSearchMatches($text, '9876543'), 'Zusätzliche Telefonkontakte müssen in der allgemeinen Fallsuche gefunden werden.');
 expectSearch(!caseSearchMatches($text, 'Rheinlandstr Stuttgart'), 'Alle Suchwörter müssen im selben Fall vorkommen.');
 expectSearch(caseSearchScore('rheinlandstr 1 5 freudenstadt', 'Rheinlandstr') > caseSearchScore('weg rheinlandstr 1 5', 'Rheinlandstr'), 'Treffer am Anfang müssen höher gewichtet werden.');
 expectSearch(casePhoneKey('+49 (0) 7367 393 97 83') === casePhoneKey('0049 7367 3939783'), 'Deutsche Rufnummern müssen unabhängig von Schreibweise übereinstimmen.');
